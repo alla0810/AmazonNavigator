80ed475942ae71ba0bb6bd453dd10571fbf52d2f
KyoSook Shin <kyosook.shin@gmail.com>
2023-09-19 23:32:46 -0500

    fetch review API implemented, but this app remembers only the last asin, needs to resolve this problem

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,7 +3,11 @@ var languageButtonsEl = document.querySelector('#language-buttons');
 var usrInputEl = document.querySelector('#SearchBar');
 var AmazonContainerEl = document.querySelector('#Amazon-container');
 var AmazonSearchTerm = document.querySelector('#Amazon-search-term');
+var reviewContainerEl = document.querySelector('#reviewContainer');
+
 var apiKey = '52de1c4d26msh3e7a0f57d6695f0p19417djsn97cd6110728d';
+
+//var asinNum = [];
 
 
   var getAmazonApi = function (keyword) {
@@ -61,7 +65,7 @@ function displayAmazonResponse(data, searchTerm)
 
   if (data.status != "OK")
   {
-    console.log("displayAmazonResponse");    
+    console.log("displayAmazonResponse Fail!");    
   }
 
   AmazonSearchTerm.textContent = searchTerm;
@@ -69,110 +73,118 @@ function displayAmazonResponse(data, searchTerm)
   var productList = data.data.products;
 
   for (var i = 0; i < productList.length; i++) {
+    var productContainer = document.createElement('div');
+    productContainer.classList = 'list-item flex-row justify-space-between align-center';
+
     var productName = productList[i].product_title;
-
-    var productEl = document.createElement('a');
-    productEl.classList = 'list-item flex-row justify-space-between align-center';
-
-    var productUrl = productList[i].product_url;
-    productEl.setAttribute('href', productUrl);
 
     var titleEl = document.createElement('span');
     titleEl.textContent = productName;
+    titleEl.innerHTML += '<br>';
 
     var productPrice = "Price: " + productList[i].product_price;
-
-    titleEl.innerHTML += '<br>';
     titleEl.innerHTML += productPrice;
 
-    productEl.appendChild(titleEl);
+    titleEl.innerHTML += '<br>';    
+    productContainer.appendChild(titleEl);
 
-    AmazonContainerEl.appendChild(productEl);
+    var productImg = new Image(100);
+    productImg.src = productList[i].product_photo;
+
+    var anchorEl = document.createElement('a');
+    anchorEl.setAttribute('href', productList[i].product_url);
+    anchorEl.appendChild(productImg);
+    productContainer.appendChild(anchorEl);
+
+    devEl = document.createElement("div");
+    var asinNum = productList[i].asin;
+    console.log("asinNum: ", productList[i].asin);
+
+    var reviewButtonEl = document.createElement('button');
+    reviewButtonEl.classList = 'review-btn';
+    reviewButtonEl.innerText = "Reviews";
+//    reviewButtonEl.type = "submit";
+    reviewButtonEl.addEventListener("click", function(event) {
+      event.preventDefault();      
+      console.log("event: ", event);
+
+      getAmazonReviewApi(asinNum);
+  
+    });
+    devEl.appendChild(reviewButtonEl);
+    productContainer.appendChild(devEl);
+
+    AmazonContainerEl.appendChild(productContainer);
   }
 
+}
 
+function getAmazonReviewApi(asinNum)
+{
+  console.log("getAmazonReviewApi");
+  console.log("asin: " + asinNum);
+
+//  const amazonApiUrl = 'https://real-time-amazon-data.p.rapidapi.com/product-reviews?asin=B07ZPKN6YR&country=US&verified_purchases_only=false&images_or_videos_only=false&page=1&page_size=10';
+  const amazonApiUrl = 'https://real-time-amazon-data.p.rapidapi.com/product-reviews?asin=' + asinNum + '&country=US&verified_purchases_only=false&images_or_videos_only=false&page=1&page_size=10';
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': apiKey,
+      'X-RapidAPI-Host': 'real-time-amazon-data.p.rapidapi.com'
+    }
+  };
+
+
+  fetch(amazonApiUrl, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Response.status");
+      }
+      return response.json();
+    })
+    .then(data => {
+
+      console.log("here!");
+      console.log(data);
+      displayAmazonReviews(data, asinNum);
+    })
+    .catch(error => console.error("Error", error));
 
 }
 
 
+function displayAmazonReviews(data, asinNum)
+{
+  console.log("displayAmazonReviews");
+  console.log(data);
 
-
-var buttonClickHandler = function (event) {
-  var language = event.target.getAttribute('data-language');
-
-  if (language) {
-    getFeaturedRepos(language);
-
-    repoContainerEl.textContent = '';
+  if (data.status != "OK")
+  {
+    console.log("displayAmazonReviews Fail!");    
   }
-};
 
-var getUserRepos = function (user) {
-  var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+  reviewContainerEl.innerHTML = "";
 
-  fetch(apiUrl)
-    .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          displayRepos(data, user);
-        });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert('Unable to connect to GitHub');
-    });
-};
-
-var getFeaturedRepos = function (language) {
-  var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
-
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayRepos(data.items, language);
-      });
-    } else {
-      alert('Error: ' + response.statusText);
+  var reviewList = data.data.reviews;  
+  if (reviewList.length === 0)
+  {
+    var cardEl = document.createElement("div");
+    cardEl.classList = 'card';
+    cardEl.textContent = 'No Review Found';
+    reviewContainerEl.appendChild(cardEl);
+  }
+  else
+  {
+   for (var i = 0; i < reviewList.length; i++) {
+      var cardEl = document.createElement("div");
+      cardEl.classList = 'card';
+      cardEl.textContent = reviewList[i].review_comment;
+      reviewContainerEl.appendChild(cardEl);
     }
-  });
-};
-
-var displayRepos = function (repos, searchTerm) {
-  if (repos.length === 0) {
-    repoContainerEl.textContent = 'No repositories found.';
-    return;
   }
 
-  repoSearchTerm.textContent = searchTerm;
+}
 
-  for (var i = 0; i < repos.length; i++) {
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
 
-    var repoEl = document.createElement('a');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
-    repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
-
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
-  }
-};
 
 userFormEl.addEventListener('submit', formSubmitHandler);
